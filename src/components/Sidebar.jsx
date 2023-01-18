d694c80547d00203c4be1313122add6728f99499
Aditya <thisisaadityaa@gmail.com>
2023-01-18 17:50:08 +0530

    ...

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,9 +2,9 @@ import { Stack } from '@mui/material';
 
 import { categories } from '../utils/constants';
 
-const selectedCategory = 'New';
 
-const Sidebar = () => (
+
+const Sidebar = ({selectedCategory, setSelectedCategory }) => (
 
     <Stack
         direction="row"
@@ -18,6 +18,7 @@ const Sidebar = () => (
         {categories.map((category) => (
             <button
                 className="category-btn"
+                onClick={() => setSelectedCategory(category.name)}
                 style={{
                     background: category.name === selectedCategory && '#FC1503',
                     color: 'white'
