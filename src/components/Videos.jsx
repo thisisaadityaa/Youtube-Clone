3468f372705aad33b0b86985028befb5fc9c30cd
Aditya <thisisaadityaa@gmail.com>
2023-01-19 21:07:11 +0530

    adding components

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,32 +1,21 @@
-import { Stack, Box} from '@mui/material';
-import {VideoCard, ChannelCard } from './';
+import React from "react";
+import { Stack, Box } from "@mui/material";
+
+import { ChannelCard, VideoCard } from "./";
 
 const Videos = ({ videos }) => {
-    
-    return(
-        <Stack direction="row" flexWrap="wrap" justifyContent="start" gap={2}>
+    // if (!videos?.length) return <Loader />;
+
+    return (
+        <Stack direction={ "row"} flexWrap="wrap" justifyContent="start" alignItems="start" gap={2}>
             {videos.map((item, idx) => (
-                <Box key ={idx}>
+                <Box key={idx}>
                     {item.id.videoId && <VideoCard video={item} />}
-                    {/* {item.id.channelId && <ChannelCard channelDetail={item} />} */}
-
-
+                    {item.id.channelId && <ChannelCard channelDetail={item} />}
                 </Box>
-
-
             ))}
-
-
-
         </Stack>
-
-
-
-
-    )
-
-
-
+    );
 }
 
-export default Videos
+export default Videos;
