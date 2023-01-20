8ad3b26ba2ce72d94d799081b9b070f711e7904c
Aditya <thisisaadityaa@gmail.com>
2023-01-20 12:07:24 +0530

    ...

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -9,7 +9,13 @@ const ChannelCard = ({ channelDetail }) => (
   <Box
     sx={{
       boxShadow: 'none',
-      borderRadius: '20px',
+     borderRadius: '20px',
+     display: 'flex',
+     justifyContent: 'center',
+     alignItems: 'center',
+     width: {xs: '356px', md: '320px'},
+     height: '326px',
+     margin: 'auto'
     }}
 
 
